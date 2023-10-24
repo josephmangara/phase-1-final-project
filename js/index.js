c93157f7a8c51b04aff85d11973c740acf01626b
josephmangara <josephmangara2@gmail.com>
2023-10-24 21:53:06 +0300

    Ediiting css

--- js/index.js
+++ js/index.js
@@ -1,16 +1,27 @@
 //The js code fetches data from a json server and displays it and creates an interactive user inferface. 
 
-docu
+document.addEventListener("DOMContentLoaded", function (){
+  getCharacters()
+});
+ function getCharacters(){
+    fetch("http://localhost:3000/results")
+    .then(res => res.json())
+    .then(data => {displayCharacters(data)
+  })
+  .catch(error => {console.error("Error:", error);
+ })
+ };
+
 //This function creates a list of the chosen characters. 
 function displayCharacters(data){
-    for (let details of data){
+    for (details of data){
     console.log(details);
     let list = document.getElementById("characters");
     let li = document.createElement("li");
     li.textContent = details.name;
     li.addEventListener("click", () => seeCharacters(details));
     list.appendChild(li);
-    
+   
 }
 }
 
@@ -26,6 +37,7 @@ function seeCharacters(details){
       <p>Gender: ${details.gender}</p>
       <p>Location: ${details.location.name}</p>
       <p>Origin: ${details.origin.name}</p>
+    </ol>
     `
 }
 
